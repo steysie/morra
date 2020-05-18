93be1641efc81b209bd44d65d67482b5624274a5
fostroll <fostroll@gmail.com>
2020-05-18 09:32:05 +0300

    example added

---
+++ examples/disassemble_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Morra project
+#
+# Copyright (C) 2019-present by Sergei Ternovykh
+# License: BSD, see LICENSE for details
+"""
+Example: Disassemble a complete model to it's parts.
+"""
+from morra import MorphParser3
+
+MODEL_FN = 'model.pickle'
+
+mp = MorphParser3()
+mp.load(MODEL_FN)
+
+if mp._cdict:
+    mp._save_cdict('_cdict.pickle')
+if mp._pos_model
+    mp._save_pos_model('_model.pos.pickle')
+if mp._pos_rev_model:
+    mp._save_pos_rev_model('_model.pos_rev.pickle')
+if mp._pos2_model:
+    mp._save_pos2_model('_model.pos2.pickle')
+if mp._feats_models:
+    mp._save_feats_models('_models.feats.pickle')
+if mp._feats_rev_models:
+    mp._save_feats_rev_models('_models.feats_rev.pickle')
+if mp._feats2_models:
+    mp._save_feats2_models('_models.feats2.pickle')
+if mp._feats_model:
+   mp._save_feats_model('_model.feats.pickle')
+if mp._feats_rev_model:
+   mp._save_feats_rev_model('_model.feats_rev.pickle')
+if mp._feats2_model:
+   mp._save_feats2_model('_model.feats2.pickle')
---
+++ examples/ne_disassemble_model.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Morra project
+#
+# Copyright (C) 2019-present by Sergei Ternovykh
+# License: BSD, see LICENSE for details
+"""
+Example: Disassemble a complete Morra NER model to it's parts.
+"""
+from morra import MorphParserNE
+
+MODEL_FN = 'model_ne.pickle'
+
+mp = MorphParserNE()
+mp.load(MODEL_FN)
+
+if mp._ne_model:
+    mp._load_ne_model('_model.ne.pickle')
+if mp._ne_rev_model:
+    mp._load_ne_rev_model('_model.ne_rev.pickle')
+if mp._ne_model:
+    mp._load_ne2_model('_model.ne2.pickle')
+if mp._ne_models:
+    mp._load_ne_models('_models.ne.pickle')
+if mp._ne_rev_models:
+    mp._load_ne_rev_models('_models.ne_rev.pickle')
+if mp._ne2_models:
+    mp._load_ne2_models('_models.ne2.pickle')
